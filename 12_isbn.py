d23c4b968cff73fa91bc331483dd3abebca02744
Ildefonso Albares Garcia <albaresgarcia.ildefonso@gmail.com>
2024-11-08 13:46:20 +0100

    Add files via upload
    08/11/2024

--- 12_isbn.py
+++ 12_isbn.py
@@ -1,6 +1,14 @@
 #Comprobacion de ISBN
+import sys
+
 print("\n***Comprobacion de ISBN***")
 num = input("Introduce el número ISBN: ")
+
+#Aquí comprobamos que la longitud sea de 10 con la funcion len()
+if len(num)!=10:
+    print("ISBN debe ser de 10 dígitos")
+    sys.exit()
+
 resultado=0
 for i in range(1,11):
     resultado = resultado+(int(num[i-1])*i)
---
+++ 14_serie_fibonacci.py
@@ -0,0 +1,16 @@
+#Calculo serie Fibonacci
+print("\n***Desarrollo seri de Fibonacci***")
+num = input("Introduce el número: ")
+numbef=0
+numcurrent=1
+resultado=""
+
+
+while numcurrent < int(num):
+    resultado= resultado + (f"{numcurrent}\n")
+    temp= numcurrent
+    numcurrent = numcurrent + numbef
+    numbef = temp
+
+print(resultado)
+
