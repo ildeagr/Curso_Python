a1583dde172590abec33157708ba3a3347caee11
Ildefonso Albares Garcia <albaresgarcia.ildefonso@gmail.com>
2024-11-08 08:59:08 +0100

    Add files via upload
    07/11/2024

---
+++ 02_operadores.py
@@ -0,0 +1,22 @@
+num1= 30
+num2= 3
+
+#Multiplicacion
+resultado = num1*num2
+
+#Valores de una division
+resultado2 = num1 / num2
+#Obtenemos la division con decimales
+print(resultado)
+#Obtenemos el cociente
+print(num1//num2)
+#Obtenemos el resto
+print(num1%num2)
+
+"""La prioridad en operaciones se siguen las reglas normativas de matemática
+
+Primero: ()
+Segundo: *, /
+Tercero: +, -
+
+"""
---
+++ 03_redondeo.py
@@ -0,0 +1,12 @@
+#Redondeo de números
+num1= 20
+num2= 3
+resultado = round((num1 / num2),2)
+print(resultado)
+
+from math import floor,ceil #Importacion de la clase Math
+
+resultado1 = floor(num1 / num2)
+print(resultado1)
+resultado2 = ceil(num1/num2)
+print(resultado2)
