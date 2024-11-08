a75830e7fe01630e1d3544728d9b55a2b5b7bfa7
Ildefonso Albares Garcia <albaresgarcia.ildefonso@gmail.com>
2024-11-08 12:04:10 +0100

    Add files via upload
    08/11/2024

--- 10_conjetura_collatz.py
+++ 10_conjetura_collatz.py
@@ -1,10 +1,13 @@
 #Calculo de la conjetura de Collatz
 print("***Calculo conjetura de Collatz***")
 num = int(input("Introduce un número: "))
+valor =""
 
 while num != 1:
     if num%2==0:
         num=num/2
     else:
         num=num*3+1
-    print(int(num))
+    valor=valor+(str(int(num))+"\n")
+
+print(valor)
